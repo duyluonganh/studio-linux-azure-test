ee4af7773e0247f8fde2ea222ad5980c93843ea0
Duy Luong <duyluong@kms-technology.com>
2022-02-09 11:29:43 +0700

    First commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ecab16fb-96e6-4ed7-b039-529f5c54d6fe</testSuiteGuid>
+   <testCaseLink>
+      <guid>c624dd7f-a5e1-4e37-9eb2-416b7417b173</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>943e6697-87a1-4b22-a18f-71fe6a51a380</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New Test Data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>943e6697-87a1-4b22-a18f-71fe6a51a380</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>a</value>
+         <variableId>2cb452cd-f1a3-45be-9bca-cd895e81f0e9</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
