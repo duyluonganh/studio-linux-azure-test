bd875a0237a4525dc01f10c0548b03cdee3a1e3c
Duy Luong <duyluong@kms-technology.com>
2022-02-09 11:44:39 +0700

    Update

--- Scripts/New Test Case/Script1644379850328.groovy
+++ Scripts/New Test Case/Script1644379850328.groovy
@@ -12,12 +12,15 @@ import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('katalon.com')
+
+DriverFactory.getWebDriver().close()
 
 WebUI.closeBrowser()
 
