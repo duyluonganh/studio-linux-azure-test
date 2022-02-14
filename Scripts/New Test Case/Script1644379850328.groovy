9753c155b14209e67fc9fd33cab9e9ca894c8852
Duy Luong <duyluong@kms-technology.com>
2022-02-14 16:24:53 +0700

    Update

--- Scripts/New Test Case/Script1644379850328.groovy
+++ Scripts/New Test Case/Script1644379850328.groovy
@@ -1,18 +1,13 @@
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.DesiredCapabilities
 
 import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory2
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.selenium.driver.CChromeDriver
 
-System.setProperty(DriverFactory.CHROME_DRIVER_PATH_PROPERTY_KEY, DriverFactory.getChromeDriverPath());
-CChromeDriver driver = new CChromeDriver(new DesiredCapabilities(), 0)
+WebDriver driver = DriverFactory2.openWebDriver()
 
 DriverFactory.changeWebDriver(driver)
 WebUI.closeBrowser()
-//
-//if (OSUtil.isUnix()) {
-//	'killall -9 chromedriver'.execute()
-//	def list = 'ps -aux | grep -i  chromedriver'.execute()
-//	println list.in.text	
-//}
